95adbda1d4855787c30d79c59ce636bbc22501f0
Clodos04 <panfilomeno.ov@gmail.com>
2026-07-29 20:55:56 -0600

    Update route handling for serving React app

--- server.cjs
+++ server.cjs
@@ -90,7 +90,7 @@ app.post('/api/tickets', (req, res) => {
 // Sirve la carpeta 'dist' generada por Vite
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
