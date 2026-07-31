b531b0c4d89639581fd94f0fb714e90678cb8d6f
Clodos04 <panfilomeno.ov@gmail.com>
2026-07-30 19:32:07 -0600

    Add initial groups data in gruposData.cjs

---
+++ gruposData.cjs
@@ -0,0 +1,14 @@
+// gruposData.cjs
+const gruposIniciales = [
+  "TELEFONIA, COMUNICACIONES Y REDES",
+  "DESARROLLO DE SOFTWARE",
+  "CONTROL ESTADISTICO",
+  "SOPORTE TECNICO",
+  "BASES E INFORMES",
+  "CLAVES",
+  "FOLIOS BIT",
+  "MANTENIMIENTO",
+  "CENTINELA"
+];
+
+module.exports = { gruposIniciales };
